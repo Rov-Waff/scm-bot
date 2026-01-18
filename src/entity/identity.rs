97ebaa27354182d444b1774c8e9eb9c8e7fc37e4
Rov_Waff <whanxiao885@gmail.com>
2026-01-18 21:41:56 +0800

    feat: implement token retrieval and enhance Stroage structure

--- src/entity/identity.rs
+++ src/entity/identity.rs
@@ -38,3 +38,79 @@ pub async fn get_captcha_id(client: &Client) -> String {
         .ticket;
     res
 }
+
+/*
+Auth Example:
+```json
+{
+  "auth": {
+    "token": "",
+    "phone_number": "",
+    "email": "",
+    "has_password": true,
+    "is_weak_password": false
+  },
+  "user_info": {
+    "id": 2615505,
+    "nickname": "xiaole233",
+    "avatar_url": "https://cdn-community.bcmcdn.com/47/community/d2ViXzEwMDFfMjYxNTUwNV8yNjE1NTA1XzE3NDczOTU2NzM5MjlfNDY0NWQwZDM.png",
+    "fullname": "",
+    "birthday": ,
+    "sex": 1,
+    "qq": "",
+    "description": "",
+    "grade": 10,
+    "grade_desc": ""
+  }
+}
+```
+*/
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Auth {
+    token: String,
+}
+#[derive(Deserialize, Serialize, Debug)]
+pub struct LoginResponse {
+    auth: Auth,
+}
+
+pub async fn get_token(client: &Client, captcha_ticket: &String) -> String {
+    let mut req_body = HashMap::new();
+    /*
+    Request Body:
+    ```
+    {
+        "identity": "手机号",
+        "password": "密码",
+        "pid": "65edCTyg"
+    }
+    ```
+    */
+    req_body.insert("identity", {
+        match std::env::var("USERNAME") {
+            Ok(r) => r,
+            Err(_) => "114514".to_string(),
+        }
+    });
+    req_body.insert("password", {
+        match std::env::var("PASSWORD") {
+            Ok(r) => r,
+            Err(_) => "r".to_string(),
+        }
+    });
+    req_body.insert("pid", "65edCTyg".to_string());
+
+    let res = client
+        .post("https://api.codemao.cn/tiger/v3/web/accounts/login/security")
+        .json(&req_body)
+        .header("X-Captcha-Ticket", captcha_ticket.to_string())
+        .send()
+        .await
+        .expect("无法获取Token")
+        .json::<LoginResponse>()
+        .await
+        .unwrap()
+        .auth
+        .token;
+    res
+}
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ pub mod entity {
 
 struct Stroage {
     ticket_id: String,
+    token:String
 }
 
 use dotenvy::dotenv;
 use log::info;
 use reqwest::ClientBuilder;
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 
 use crate::entity::identity;
 
@@ -18,9 +19,7 @@ use crate::entity::identity;
 async fn main() {
     dotenv().ok();
     env_logger::init();
-    let mut stroage = Stroage {
-        ticket_id: "".to_string(),
-    };
+    let mut stroage = Stroage {ticket_id:"".to_string(), token: "".to_string() };
     let client = ClientBuilder::new()
         .timeout(Duration::from_secs(500))
         .build()
@@ -28,5 +27,9 @@ async fn main() {
 
     // 获取CaptchaTicket(阻塞)
     stroage.ticket_id = identity::get_captcha_id(&client).await;
-    info!("获取到CAPTCHA ID{}", &stroage.ticket_id)
+    info!("获取到CAPTCHA ID{}", &stroage.ticket_id);
+    stroage.token = identity::get_token(&client, &stroage.ticket_id).await;
+    info!("成功获取token:{}",&stroage.token);
+    //包进Arc,异步使用
+    let stroage = Arc::new(stroage);
 }
